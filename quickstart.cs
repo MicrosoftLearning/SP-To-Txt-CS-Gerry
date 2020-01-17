0166688a4eceda713f90b19e89b8f5cfd0b7377a
Gerry O'Brien <gerry.obrien@microsoft.com>
2020-01-17 08:57:31 -0800

    Create quickstart.cs

---
+++ quickstart.cs
@@ -0,0 +1,52 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+//
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            await RecognizeSpeechAsync();
+        }
+
+        static async Task RecognizeSpeechAsync()
+        {
+            var config = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
+
+            using (var audioInput = AudioConfig.FromWavFileInput("whatstheweatherlike.wav"))
+            using (var recognizer = new SpeechRecognizer(config, audioInput))
+            {
+                Console.WriteLine("Recognizing first result...");
+                var result = await recognizer.RecognizeOnceAsync();
+
+                switch (result.Reason)
+                {
+                    case ResultReason.RecognizedSpeech:
+                        Console.WriteLine($"We recognized: {result.Text}");
+                        break;
+                    case ResultReason.NoMatch:
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        break;
+                    case ResultReason.Canceled:
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+                
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
